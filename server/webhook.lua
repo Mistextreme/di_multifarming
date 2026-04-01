2c98f265da278da7f8c20323113d1a42f5a1356d
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2026-04-01 10:48:37 +0100

    Upload: webhook.lua

---
+++ server/webhook.lua
@@ -0,0 +1,13 @@
+Config.Webhooks = {
+    strawberry = "",
+    blueberry  = "",
+    orange     = "",
+    lemon   = "",
+    tomato     = "",
+    pumpkin = "",
+    grapes = "",
+    apple = "",
+    bulk_sell = "",
+    buy =  ""
+}
+
